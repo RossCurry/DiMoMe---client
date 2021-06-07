c385a1fefd60cdc0f243bb2108e7b4be384af4b5
Ross Curry <ross_curry@yahoo.co.uk>
2021-06-07 19:02:22 +0200

    refactor: login comp refactored to TS

--- src/components/login/Login.component.tsx
+++ src/components/login/Login.component.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Authentication from '../../containers/authentication/Authentication.container';
 
-function Login({ match }) {
+// TODO I can't find where this prop is coming from
+type match = {
+  isExact: boolean;
+};
+export interface LoginProps {
+  match: match;
+}
+
+const Login = ({ match }: LoginProps): JSX.Element => {
   return (
     <div className="subscribe-container">
       <Authentication subscribe={!match.isExact} />
     </div>
   );
-}
+};
 
 export default Login;
