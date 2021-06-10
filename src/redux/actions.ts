54a905ba0ba5a23d34fcbc40ff7add46607509c9
Ross Curry <ross_curry@yahoo.co.uk>
2021-06-10 17:07:54 +0200

    refactor: add new action type to actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,19 +1,26 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { userFromDB } from '../ApiService';
+
+type categoryAction = {
+  type: 'ADD_CATEGORY' | 'EDIT_CATEGORY' | 'DELETE_CATEGORY';
+  payload: string | number;
+};
+
 // CATEGORY
-export const addCategory = (newCategory: string): PayloadAction<string> => ({
+export const addCategory = (newCategory: string): categoryAction => ({
   type: 'ADD_CATEGORY',
   payload: newCategory,
 });
-export const editCategory = (newCategory: string): PayloadAction<string> => ({
+export const editCategory = (newCategory: string): categoryAction => ({
   type: 'EDIT_CATEGORY',
   payload: newCategory,
 });
-export const deleteCategory = (categoryId: number): PayloadAction<number> => ({
+export const deleteCategory = (categoryId: number): categoryAction => ({
   type: 'DELETE_CATEGORY',
   payload: categoryId,
 });
 
+// TODO add user action type
 // USER
 export const updateUser = (user: userFromDB): PayloadAction<userFromDB> => ({
   type: 'UPDATE_USER',
